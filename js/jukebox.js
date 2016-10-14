cf9735788a23e8fde8c8e23a9f0f6392d395d76f
bharti gurnaney <bharti0717@yahoo.com>
2016-10-14 00:40:14 +0000

    Done.

--- js/jukebox.js
+++ js/jukebox.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
   menu();
   play();
@@ -7,7 +8,7 @@ $(document).ready(function(){
 
 var jukeboxSongs = { "taylor swift": "in the clear", "beyonce": "halo", "drake": "hotline bling"};
 var artists = Object.keys(jukeboxSongs);
-var theArtist = artists[Math.floor(Math.random()) * artists.length];
+var theArtist = artists[randomNumber() * artists.length];
 var songToPlay = jukeboxSongs[theArtist];
 
 function randomNumber() {
@@ -15,30 +16,52 @@ function randomNumber() {
 }
 
 function menu() {
-  $('#menu').on('click', function(e) {
+  $('#menu').on('click',addtomenu) ;
+
+  /*function(e) {
     $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  });
+  });*/
 }
 
+function addtomenu()
+{
+  $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
+}
 function play() {
-  $('#play').on('click', function(e) {
+  $('#play').on('click',function(e) {
     $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
     return "now playing " + songToPlay + " by " + theArtist;
   });
 }
 
 function pause() {
-  $('#pause').on('click', function(e) {
+  $('#pause').on('click', addtopause);
+  /*function(e) {
     $('#songPaused').html(songToPlay + " is paused");
-  });
+  });*/
 }
 
+function addtopause()
+{
+  $('#songPaused').html(songToPlay + " is paused");
+}
 function show() {
-  $('#show').on('click', function(e) {
+  $('#show').on('click',addtoshow); 
+  /*function(e) {
     var songs = "";
     for (var key in jukeboxSongs) {
       songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
     }
     $('#showSongs').html(songs);
-  });
+  });*/
+}
+
+function addtoshow()
+{
+  var songs = "";
+  for (var key in jukeboxSongs) {
+    songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
+  }
+  $('#showSongs').html(songs);
+
 }
